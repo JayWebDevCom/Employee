fca7be96fecb0f50316f604af5aac7030abb62c9
JayWebDevCom <jaiye.s@gmail.com>
2017-07-18 14:33:13 +0100

    Add Employee class add name getter

--- src/Employee.java
+++ src/Employee.java
@@ -7,4 +7,14 @@ class Employee {
     private String name;
     private double Salary;
     private LocalDate hireDay;
+
+    public Employee(String name, double salary, int year, int month, int day) {
+        name = name;
+        salary = salary;
+        hireDay = LocalDate.of(year, month, day);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
